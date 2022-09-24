33662f9e10db1e40f1ac8692e2513c2d97d0833e
chris-machado <cmachado11@gmail.com>
2022-09-24 12:51:49 -0400

    added files

---
+++ mysqlDocker/MBTA.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE IF NOT EXISTS MBTAdb;
+
+USE MBTAdb;
+
+DROP TABLE IF EXISTS mbta_buses;
+
+CREATE TABLE mbta_buses (
+    record_num INT AUTO_INCREMENT PRIMARY KEY,
+    id varchar(255) not null,
+    latitude decimal(11,8) not null,
+    longitude decimal(11,8) not null,
+    bearing INT default NULL,
+    current_status VARCHAR(20) default NULL,
+    current_stop_sequence INT default NULL,
+    direction_id INT default NULL,
+    label VARCHAR(20) default NULL,
+    occupancy_status VARCHAR(20) default NULL,
+    speed INT default NULL,
+    updated_at DATETIME default NULL
+);
+
+test
