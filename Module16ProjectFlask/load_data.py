540e0c0c219685c93e01fb20da5b60945929d5db
chris-machado <cmachado11@gmail.com>
2022-09-27 17:35:21 -0400

    defined type characters

--- Module16ProjectFlask/load_data.py
+++ Module16ProjectFlask/load_data.py
@@ -5,16 +5,16 @@ df = pd.read_csv('mbta.csv', low_memory=False)
 
 mbtaDict = {}
 for ind, row in df.iterrows():
-    mbtaDict['id'] = row.iloc[2], 
-    mbtaDict['latitude'] = row.iloc[3], 
-    mbtaDict['longitude'] = row.iloc[4], 
-    mbtaDict['bearing'] = row.iloc[5], 
-    mbtaDict['current_status'] = row.iloc[6],
-    mbtaDict['current_stop_sequence'] = row.iloc[7], 
-    mbtaDict['direction_id'] = row.iloc[8],
-    mbtaDict['label'] = row.iloc[9], 
-    mbtaDict['occupancy_status'] = row.iloc[10],
-    mbtaDict['speed'] = row.iloc[11], 
+    mbtaDict['id'] = row.iloc[2]
+    mbtaDict['latitude'] = float(row.iloc[3]) 
+    mbtaDict['longitude'] = float(row.iloc[4]) 
+    mbtaDict['bearing'] = int(row.iloc[5]) 
+    mbtaDict['current_status'] = row.iloc[6]
+    mbtaDict['current_stop_sequence'] = int(row.iloc[7])
+    mbtaDict['direction_id'] = int(row.iloc[8])
+    mbtaDict['label'] = row.iloc[9],
+    mbtaDict['occupancy_status'] = row.iloc[10]
+    mbtaDict['speed'] = int(row.iloc[11]) 
     mbtaDict['updated_at'] = row.iloc[12]
 
     mysqldb.insertMBTARecord(mbtaDict)
